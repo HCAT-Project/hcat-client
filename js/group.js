cba9a7a8c051159e3f16f9ecd8dffb62e8b95224
hsn <hsn1919810@gmail.com>
2023-04-01 23:35:41 +0800

    更新版权

--- js/group.js
+++ js/group.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2023. HCAT-Project-Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // 新建群组
 function createGroup(){
     mdui.prompt('请输入群聊名称', '创建群组',
