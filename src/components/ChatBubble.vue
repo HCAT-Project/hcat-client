d262d2bea0205d58dafd230ed2395c77ce875d3b
Roy <lyt2980999208@gmail.com>
2023-04-16 14:56:37 +0800

    feat: img preview

--- src/components/ChatBubble.vue
+++ src/components/ChatBubble.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { onClickOutside } from '@vueuse/core'
 import type { MsgChain } from '~/types'
 
 withDefaults(defineProps<{
@@ -7,6 +8,11 @@ withDefaults(defineProps<{
   time: string
 }>(), {
   fromSelf: false,
+})
+const imgPreviewVisible = ref(false)
+const imgPreview = ref<HTMLImageElement | null>(null)
+onClickOutside(imgPreview, (_) => {
+  imgPreviewVisible.value = false
 })
 </script>
 
@@ -17,7 +23,11 @@ withDefaults(defineProps<{
       <div v-if="message.type === 'text'" break-all>
         {{ message.msg }}
       </div>
-      <img v-else :src="message.msg" max-w-40>
+      <img v-else :src="message.msg" max-w-40 cursor-pointer @click="imgPreviewVisible = true">
+      <!-- ImgPreview -->
+      <div v-if="imgPreviewVisible" bg="black op40" absolute z-50 inset-0 flex items-center justify-center>
+        <img ref="imgPreview" max-w-screen-sm :src="message.msg">
+      </div>
       <div flex justify-between items-center gap-10>
         <p text="xs text-secondary">
           {{ time }}
