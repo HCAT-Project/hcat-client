f6711ce3d63d7bec48fe15af9bc75428c4588050
Roy <lyt2980999208@gmail.com>
2023-10-20 22:48:48 +0800

    feat: mobile btn

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,11 +3,10 @@
 </script>
 
 <template>
-  <div w-full h-full flex="~ col" items-center justify-center gap2>
-    <img src="/logo.png" w-20 h-20>
-    <p text="2xl primary" font-bold>
-      H<span text-light>CAT</span>
-    </p>
+  <div w-full h-full flex="~ col" items-center  gap2>
+    <PlaceholderPage
+    title="HCAT"
+    />
   </div>
 </template>
 
